69f8bf69d1d8d23866400c41ab849501ed9d3b26
Hook-And <osya_stalin_1878@mail.ru>
2025-10-25 14:15:46 +0300

    deleted old migrations

--- database/migrations/2025_10_25_070713_remove_surname_from_users_table.php
+++ database/migrations/2025_10_25_070713_remove_surname_from_users_table.php
@@ -8,17 +8,9 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            // Удаляем столбец surname
-            $table->dropColumn('surname');
-        });
     }
 
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            // Если нужно, восстанавливаем столбец (например, как nullable)
-            $table->string('surname')->nullable();
-        });
     }
 };
