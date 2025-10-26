c82915764d542f5cd6fd23204242568d447439e0
Hook-And <osya_stalin_1878@mail.ru>
2025-10-26 06:53:23 +0300

    reverted changes

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\SiteContent;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Artisan;
 
 class AdminController extends Controller
 {
@@ -14,11 +13,6 @@ class AdminController extends Controller
      */
     public function index()
     {
-        Artisan::call('db:seed', [
-            '--class' => 'Database\\Seeders\\SiteContentSeeder', // Укажи свой класс сидера
-            '--force' => true, // нужно, если в production
-        ]);
-
         $sections = [
             'header' => 'Заголовок',
             'purpose' => 'Цель канала',
